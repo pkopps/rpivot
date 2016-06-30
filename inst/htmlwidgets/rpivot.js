10cf3c9dcf3a61986042066b8d47f4183591de3a
pkopps <patrickkopps@gmail.com>
2016-06-30 13:48:24 -0400

    Update rpivot.js

--- inst/htmlwidgets/rpivot.js
+++ inst/htmlwidgets/rpivot.js
@@ -14,7 +14,7 @@ HTMLWidgets.widget({
 	    x.data = HTMLWidgets.dataframeToD3(x.data);
 
 	    var derivers = $.pivotUtilities.derivers;
-     	var tpl = $.pivotUtilities.aggregators;
+     	var tpl = $.pivotUtilities.aggregatorTemplates;
       
       // set locale to "en" which is the default for pivottable
       //  this eases code later
