ad5d2f323cd659e6c3f82a205c5989793447ca45
pkopps <patrickkopps@gmail.com>
2016-07-21 16:09:40 -0400

    Update rpivot.js

--- inst/htmlwidgets/rpivot.js
+++ inst/htmlwidgets/rpivot.js
@@ -59,14 +59,16 @@ HTMLWidgets.widget({
       
       $(function(){
       	
-      	var numsAfterDec = x.params.numsAfterDec;
+      	//var numsAfterDec = x.params.numsAfterDec;
 	var utils = $.pivotUtilities;
 	
+	/*
 	var functs = {
 	        'Sum over Sum': $.pivotUtilities.aggregatorTemplates.sumOverSum,
 	        Sum: $.pivotUtilities.aggregatorTemplates.sum,
 	        Average: $.pivotUtilities.aggregatorTemplates.average,
         }
+        */
 	
 	      $('#'+el.id).pivot(
 	      		x.data,
@@ -74,7 +76,8 @@ HTMLWidgets.widget({
 	      		rows: x.params.rows,
     			cols: x.params.cols,
     			// TESTING aggregator
-    			aggregator: functs[x.params.funct](utils.numberFormat({digitsAfterDecimal: numsAfterDec}))(x.params.vals),
+    			aggregator: utils.aggregatorTemplates[x.params.funct](x.params.numberFormat)(x.params.vals),
+    			//aggregator: functs[x.params.funct](utils.numberFormat({digitsAfterDecimal: numsAfterDec}))(x.params.vals),
     			//aggregator: utils.aggregators[x.params.funct](x.params.vals),
     			renderer: utils.renderers[x.params.renderer]
 	      		},
