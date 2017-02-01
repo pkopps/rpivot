750b4b9b0240c5f9d0eab7815ff1abd9971df1b3
pkopps <patrickkopps@gmail.com>
2017-02-01 18:27:28 -0500

    Update rpivot.R

--- R/rpivot.R
+++ R/rpivot.R
@@ -117,26 +117,26 @@ rpivot <- function(
       params = params
     )
 
+htmlwidgets::createWidget(
+  name = 'rpivot',
+  x,
+  width = width,
+  height = height,
+  sizingPolicy = htmlwidgets::sizingPolicy(
+    knitr.figure = FALSE,
+    knitr.defaultHeight = '100%',
+    knitr.defaultWidth = '100%'
+  ),
+  package = 'rpivot'
+)
+      
 # htmlwidgets::createWidget(
 #   name = 'rpivot',
 #   x,
 #   width = width,
 #   height = height,
-#   sizingPolicy = htmlwidgets::sizingPolicy(
-#     knitr.figure = FALSE,
-#     # knitr.defaultHeight = '100%',
-#     # knitr.defaultWidth = '100%'
-#   ),
 #   package = 'rpivot'
 # )
-      
-    htmlwidgets::createWidget(
-      name = 'rpivot',
-      x,
-      width = width,
-      height = height,
-      package = 'rpivot'
-    )
 }
 
 #' Widget output function for use in Shiny
