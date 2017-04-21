195d7b8f59f3c0b2fa95cbdf359f617d25a3e6e5
pkopps <patrickkopps@gmail.com>
2017-04-21 16:04:53 -0400

    Update rpivot.js

--- inst/htmlwidgets/rpivot.js
+++ inst/htmlwidgets/rpivot.js
@@ -66,7 +66,7 @@ HTMLWidgets.widget({
     			cols: x.params.cols,
     			aggregator: utils.aggregatorTemplates[x.params.funct](utils.numberFormat(x.params.numberFormat))(x.params.vals),
     			renderer: utils.renderers[x.params.renderer],
-			sorters: utils.sortAs[x.params.sorters]
+			sorter: utils.sortAs()
 	      		},
 	      		true,
 	      		x.locale
